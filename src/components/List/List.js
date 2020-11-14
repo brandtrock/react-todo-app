dd7e0b181715bd1b0097267de7a7f0f23b5f87fa
Brandt <bmjorgensen@gmail.com>
2020-11-14 15:58:27 -0600

    css and stuff

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './List.css';
 
 export default class List extends React.Component {
 
@@ -7,18 +8,20 @@ export default class List extends React.Component {
 
     this.state = {
       items: [
-        { key: 0, value: 'First Item' },
-        { key: 1, value: 'Second Item' },
-        { key: 2, value: 'Third Item' }
+        { key: 0, value: 'First Item', isComplete: false },
+        { key: 1, value: 'Second Item', isComplete: false },
+        { key: 2, value: 'Third Item', isComplete: false }
       ],
       currentItem: {
         key: '',
-        name: ''
+        name: '',
+        isComplete: false
       }
     };
     this.handleInput = this.handleInput.bind(this); // ADD ITEM: THIS HAPPENS FIRST
     this.addItem = this.addItem.bind(this); // ADD ITEM: THIS HAPPENS SECOND
     this.removeItem = this.removeItem.bind(this);
+    this.itemCheckbox = this.itemCheckbox.bind(this);
   }
 
   handleInput(e) {
@@ -40,7 +43,8 @@ export default class List extends React.Component {
         items: items,
         currentItem: {
           key: '',
-          value: ''
+          value: '',
+          isComplete: false
         }
       });
     }
@@ -53,12 +57,26 @@ export default class List extends React.Component {
     });
   }
 
+  itemCheckbox(key) {
+    // TODO: Finish getting this to work, maybe just use React Hooks?
+    const tempItems = [...this.state.items]; // make a shallow copy of the items
+    const tempItem = {...tempItems[key]}; // make a shallow copy of the item to mutate
+
+    tempItem.isComplete = true; // mutate the property
+    tempItems[key] = tempItem; // put the item back into the array
+
+    this.setState({
+      items: tempItems
+    }); // Set state with the new copy
+    console.log('state 2: ', this.state);
+  }
+
   render() {
     return(
       <div className="list">
         <h1>My Todo App</h1>
         <form onSubmit={this.addItem}>
-          <div className="form-group">
+          <div className="input-group">
             <input
               className="form-control"
               type="text"
@@ -66,16 +84,22 @@ export default class List extends React.Component {
               value={this.state.currentItem.value}
               onChange={this.handleInput}
             />
+            <span className="input-group-btn">
+              <button className="btn btn-default" type="submit">Add</button>
+            </span>
           </div>
         </form>
-        <ul className="list-group">
+        <ul className="list-group row">
           {this.state.items.map((item) => (
-            <li className="list-group-item" key={item.key}>
+            <li className="list-group-item col-md-8" key={item.key}>
+              <span className="item-checkbox">
+                <input type="checkbox" onClick={() => this.itemCheckbox(item.key)} />
+              </span>
+              <span className="list-item">{item.value}</span>
               <span
                 onClick={() => this.removeItem(item.key)}
-                style={{ textDecoration: "underline", cursor: "pointer" }}
-              >[REMOVE]</span>{" "}
-              {item.value}
+                className="badge remove-item"
+              >{"X"}</span>
             </li>
           ))}
         </ul>
